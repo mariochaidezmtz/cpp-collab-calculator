4626706d658b55237fa80d53fb797a6a1aad7a53
Gustavo Cordova <guscordovaa06@gmail.com>
2025-11-19 11:45:13 -0700

    Update operations.cpp
    Add multiplication and division

--- operations.cpp
+++ operations.cpp
@@ -2,3 +2,11 @@
 
 double add(double a, double b) { return a + b; }
 double subtract(double a, double b) { return a - b; }
+double multiply(double a, double b) { return a * b; }
+double divide(double a, double b) {
+if (b != 0) return a / b;
+else {
+cout << "Error: División por cero\n";
+return 0;
+}
+}
